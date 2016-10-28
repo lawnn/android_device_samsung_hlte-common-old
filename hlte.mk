5be1b839be99e316513ac43388a90a90938df509
lawn <bloom.all.over.12@gmail.com>
2016-10-29 05:07:06 +0900

    Remove GPS

--- hlte.mk
+++ hlte.mk
@@ -76,11 +76,6 @@ PRODUCT_PACKAGES += \
     libxml2 \
     Snap
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/gps.conf:/system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/flp.conf:/system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:/system/etc/sap.conf
-
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/clatd.conf:system/etc/clatd.conf \
