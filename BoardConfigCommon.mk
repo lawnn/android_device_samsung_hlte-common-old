caf1a6359a175edc798deb303c83395043a4a2ce
lawn <bloom.all.over.12@gmail.com>
2016-11-05 00:22:22 +0900

    Set selinux=permissive

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,6 +31,7 @@ TARGET_PROVIDES_INIT_RC := true
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
